04ad0d4c202a3d305396dc9d18fe0716f0a65cae
Hiroaki Kitada <kita83@gmail.com>
2017-06-30 20:38:59 +0900

    first commit

---
+++ Assets/Script/DangerWall.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class DangerWall : MonoBehaviour {
+
+	void OnCollisionEnter(Collision hit) {
+		if (hit.gameObject.CompareTag("Player")) {
+			int sceneIndex = SceneManager.GetActiveScene ().buildIndex;
+
+			SceneManager.LoadScene (sceneIndex);
+		}
+	}
+}
